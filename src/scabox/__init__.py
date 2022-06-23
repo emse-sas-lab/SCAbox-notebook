b92ef436ab7e311820aea37f15df37e1b4f6ef8f
Gravellier Joseph <joseph.gravellier@gmail.com>
2022-06-23 13:10:20 +0200

    removed pypower

--- src/scabox/__init__.py
+++ src/scabox/__init__.py
@@ -4,7 +4,6 @@ import matplotlib.pyplot as plt
 from .com_serial import ZyboSerial
 from .encrypt import ZyboEncrypt
 from .smooth import SmoothFilter
-from .cpa import CPA
 from .class_cpa import ClassCPA
 from .aes import AESSelectionFct
 from .demo import Demo
